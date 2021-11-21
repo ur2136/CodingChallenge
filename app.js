9d3d357caeaff54b7c40e9871a40afa3a1d64052
Umang Raj <umangraj@Umangs-Air.lan>
2021-11-21 01:39:58 -0500

    Added ejs related dependencies and initializations

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const http = require('http');
+require('dotenv').config();
+const port = 3000;
+const app = express();
+
+//view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//public assets
+app.use(express.static('./public'));
+app.listen(port, () => console.log(`App started on ${port}`));
